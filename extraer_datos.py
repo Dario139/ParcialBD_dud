a291fb2cce47d4d861f29be146ac2a6a46c5a501
Dario139 <115668657+Dario139@users.noreply.github.com>
2023-08-29 11:19:34 -0500

    Update extraer_datos.py

--- extraer_datos.py
+++ extraer_datos.py
@@ -2,60 +2,48 @@ import boto3
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-
-def funtionL():
-    nombre = str(datetime.today().strftime('%Y-%m-%d'))
+def functionL():
+    nombre = datetime.today().strftime('%Y-%m-%d')
     s3 = boto3.resource('s3')
-    bucket = s3.Bucket('parcialbigdatacorte1')
-    obj_tiempo = bucket.Object(str("headlines/final/" +
-                                   "eltiempo-" + nombre +
-                                   ".html"))
+    bucket_name = 'parcialbigdatacorte1'
+
+    obj_tiempo = s3.Object(bucket_name, f'headlines/final/eltiempo-{nombre}.html')
     body_tiempo = obj_tiempo.get()['Body'].read()
-    obj_elespectador = bucket.Object(str("headlines/final/" +
-                                       "elespectador-" + nombre +
-                                       ".html"))
+    obj_elespectador = s3.Object(bucket_name, f'headlines/final/elespectador-{nombre}.html')
     body_elespectador = obj_elespectador.get()['Body'].read()
-  
+
     html_tiempo = BeautifulSoup(body_tiempo, 'html.parser')
-    html_elespectador = BeautifulSoup(obj_elespectador, 'html.parser')
-  
+    html_elespectador = BeautifulSoup(body_elespectador, 'html.parser')
+
     data_noticias_tiempo = html_tiempo.find_all('article')
     data_noticias_elespectador = html_elespectador.find_all('article')
-    csv_tiempo = ""
-    csv_elespectador = ""
-    linea_0 = "Nombre, Categoria, Link\n"
-    for i in range(len(data_noticias_tiempo)):
-        link = "eltiempo.com" + \
-               data_noticias_tiempo[i].find('a',
-                                            class_='title page-link')['href']
-        name = data_noticias_tiempo[i]['data-name'].replace(",", "")
-        category = data_noticias_tiempo[i]['data-seccion']
-        csv_tiempo += linea_0 + name + "," + \
-            category + "," + \
-            link + \
-            "\n"
-    for i in range(len(data_noticias_elespectador)):
-        link = "elespectador.co" + data_noticias_elespectador[i].find('a')['href']
-        name = (data_noticias_elespectador[i].find('a').text).replace(",", "")
+
+    linea_0 = "Nombre,Categoria,Link\n"
+    csv_tiempo = []
+    csv_elespectador = []
+
+    for article in data_noticias_tiempo:
+        link = "eltiempo.com" + article.find('a', class_='title page-link')['href']
+        name = article['data-name'].replace(",", "")
+        category = article['data-seccion']
+        csv_tiempo.append(f"{name},{category},{link}")
+
+    for article in data_noticias_elespectador:
+        link = "elespectador.co" + article.find('a')['href']
+        name = article.find('a').text.replace(",", "")
         category = link.split('/')[1]
-        csv_elespectador += linea_0 + name + "," + \
-            category + "," + \
-            link + \
-            "\n"
-    boto3.client('s3').put_object(Body=csv_tiempo,
-                                  Bucket='parcialbigdatacorte1',
-                                  Key=str('headlines/final' +
-                                          '/periodico=eltiempo/year=' +
-                                          nombre[:4]+'/month=' +
-                                          nombre[5:7]+'/day=' +
-                                          nombre[8:]+'/eltiempo.csv'))
-    boto3.client('s3').put_object(Body=csv_elespectador,
-                                  Bucket='parcialbigdatacorte1',
-                                  Key=str('headlines/final' +
-                                          '/periodico=elespectador/year=' +
-                                          nombre[:4]+'/month=' +
-                                          nombre[5:7]+'/day=' +
-                                          nombre[8:]+'/elespectador.csv'))
-                                          
-                                          
-funtionL()
+        csv_elespectador.append(f"{name},{category},{link}")
+
+    s3_client = boto3.client('s3')
+    csv_tiempo_content = linea_0 + '\n'.join(csv_tiempo)
+    csv_elespectador_content = linea_0 + '\n'.join(csv_elespectador)
+
+    s3_client.put_object(Body=csv_tiempo_content,
+                         Bucket=bucket_name,
+                         Key=f'headlines/final/periodico=eltiempo/year={nombre[:4]}/month={nombre[5:7]}/day={nombre[8:]}/eltiempo.csv')
+
+    s3_client.put_object(Body=csv_elespectador_content,
+                         Bucket=bucket_name,
+                         Key=f'headlines/final/periodico=elespectador/year={nombre[:4]}/month={nombre[5:7]}/day={nombre[8:]}/elespectador.csv')
+
+functionL()
