6e862707e8518c46dbe15ff8e24ae988f5c02677
Dario139 <115668657+Dario139@users.noreply.github.com>
2023-09-03 15:47:57 -0500

    Update test_descargar_paginas.py

--- test_descargar_paginas.py
+++ test_descargar_paginas.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 def test_eltiempo():
-    eltiempo = extraer()
     body_tiempo = get_html(get_url_tiempo())
     html_tiempo = BeautifulSoup(body_tiempo, 'html.parser')
     data_noticias_tiempo = html_tiempo.find_all('article')
@@ -14,10 +13,3 @@ def test_eltiempo():
      csv_tiempo.append(f"{name},{category},{link}")
     
     assert csv_tiempo[0] == "Durante 15 dias habra cierre en carriles del puente de la calle 127 con autonorte,Bogota,eltiempo.com/bogota/bogota-durante-15-dias-habra-cierre-en-carriles-del-puente-de-calle-127-con-autonorte-802268"
-
-def test_get_url_tiempo():
-    assert get_url_tiempo() == "https://www.eltiempo.com/"
-
-
-def test_get_url_elespectador():
-    assert get_url_elespectador() == "https://www.elespectador.com/"
