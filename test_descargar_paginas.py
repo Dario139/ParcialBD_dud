2780e0fd46cef3c82361935219b14820126e4acf
Dario139 <115668657+Dario139@users.noreply.github.com>
2023-09-03 14:53:18 -0500

    Update test_descargar_paginas.py

--- test_descargar_paginas.py
+++ test_descargar_paginas.py
@@ -1,11 +1,10 @@
 from datetime import datetime
-from extraer_datos import get_url_tiempo, get_date, get_url_elespectador
+from extraer_datos import extraer
 
 
-def test_get_date():
-    dt = get_date()
-    assert dt == datetime.today().strftime('%Y-%m-%d')
-
+def test_eltiempo():
+    eltiempo, elespectador = extraer()
+    assert eltiempo[0] == "Durante 15 dias habra cierre en carriles del puente de la calle 127 con autonorte,Bogota,eltiempo.com/bogota/bogota-durante-15-dias-habra-cierre-en-carriles-del-puente-de-calle-127-con-autonorte-802268"
 
 def test_get_url_tiempo():
     assert get_url_tiempo() == "https://www.eltiempo.com/"
