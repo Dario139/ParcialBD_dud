a6a68dfedd8b0d56333b46f6433221d8ddc81c4f
Dario139 <115668657+Dario139@users.noreply.github.com>
2023-09-03 15:43:41 -0500

    Update test_descargar_paginas.py

--- test_descargar_paginas.py
+++ test_descargar_paginas.py
@@ -3,8 +3,19 @@ from extraer_datos import extraer
 
 
 def test_eltiempo():
-    eltiempo, elespectador = extraer()
-    assert eltiempo[0] == "Durante 15 dias habra cierre en carriles del puente de la calle 127 con autonorte,Bogota,eltiempo.com/bogota/bogota-durante-15-dias-habra-cierre-en-carriles-del-puente-de-calle-127-con-autonorte-802268"
+    eltiempo = extraer()
+    body_tiempo = get_html(get_url_tiempo())
+    html_tiempo = BeautifulSoup(body_tiempo, 'html.parser')
+    data_noticias_tiempo = html_tiempo.find_all('article')
+
+    for article in data_noticias_tiempo:
+     link = "eltiempo.com" + article.find('a',
+                                           class_='title page-link')['href']
+     name = article['data-name'].replace(",", "")
+     category = article['data-seccion']
+     csv_tiempo.append(f"{name},{category},{link}")
+    
+    assert csv_tiempo[0] == "Durante 15 dias habra cierre en carriles del puente de la calle 127 con autonorte,Bogota,eltiempo.com/bogota/bogota-durante-15-dias-habra-cierre-en-carriles-del-puente-de-calle-127-con-autonorte-802268"
 
 def test_get_url_tiempo():
     assert get_url_tiempo() == "https://www.eltiempo.com/"
