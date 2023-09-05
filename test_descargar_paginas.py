3ea412596978da753e31e0d5238c3a6d2eab15d0
Dario139 <115668657+Dario139@users.noreply.github.com>
2023-09-05 10:11:43 -0500

    Update test_descargar_paginas.py

--- test_descargar_paginas.py
+++ test_descargar_paginas.py
@@ -2,16 +2,11 @@ from datetime import datetime
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import extraer_datos
+import descargar_paginas
+
+descargar_paginas.obtener_html()
 
 csv_tiempo = extraer_datos.extraer()
-
-def get_url_tiempo():
-    return "https://www.eltiempo.com/"
-
-def get_html(url):
-    with urlopen(url) as response:
-        html = response.read()
-    return html
     
 def test_eltiempo():    
     assert csv_tiempo[0] == "En vivo movilidad de bogota nuevos cierres por caida de material en via al llano,Bogota,eltiempo.com/bogota/via-al-llano-movilidad-en-bogota-este-5-de-septiembre-802685"
